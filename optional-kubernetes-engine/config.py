8784cbd1975702e0a9c6c353559fd88f4dca848d
PG <changux@users.noreply.github.com>
2018-06-06 13:23:42 -0500

    Update config.py

--- optional-kubernetes-engine/config.py
+++ optional-kubernetes-engine/config.py
@@ -32,7 +32,7 @@ DATA_BACKEND = 'datastore'
 
 # Google Cloud Project ID. This can be found on the 'Overview' page at
 # https://console.developers.google.com
-PROJECT_ID = 'ch-vx1s58sx726prpb8ub6dfx341hl'
+PROJECT_ID = '432726658846-bookshelf'
 
 # CloudSQL & SQLAlchemy configuration
 # Replace the following values the respective values of your Cloud SQL
